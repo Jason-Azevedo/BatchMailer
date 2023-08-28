2b9e9bb187e94f2a8569180177b64071d037f67c
Jason Azevedo <jasonazevedo03@gmail.com>
2023-08-28 20:09:41 +0200

    Implemented logger and tests

--- batch_mailer/logger.py
+++ batch_mailer/logger.py
@@ -1,28 +1,40 @@
 from os import path
 from datetime import datetime, date
 
-
-def info():
-    return
-
-
-def warn():
-    return
+INFO = "INFO"
+WARN = "WARN"
+ERROR = "ERROR"
+LOG_FOLDER_PATH = "/logs"
 
 
-def error():
-    return
+def info(text):
+    log(INFO, text)
 
 
-def write_log(text, filePath="/logs"):
+def warn(text):
+    log(WARN, text)
+
+
+def error(text):
+    log(ERROR, text)
+
+
+def log(type: str, text):
+    current_time = datetime.now().strftime("%H:%M:%S")
+    log_message = f"[{type} {current_time}]: {text}\n"
+
+    write_log(log_message)
+
+
+def write_log(text):
     # Create a new log file with the date as the file name in format: log_2023_08_21.txt
     try:
         todaysDate = date.today().strftime("%Y_%m_%d")
         logFileName = f"log_{todaysDate}.txt"
-        absoluteLogFilePath = path.join(path.abspath(filePath), logFileName)
+        absoluteLogFilePath = path.join(path.abspath(LOG_FOLDER_PATH), logFileName)
 
-        with open(absoluteLogFilePath, "w") as logFile:
-            logFile.write(text)
+        with open(absoluteLogFilePath, "a") as logFile:
+            logFile.write(text + "\n")
 
     except Exception as ex:
         print("logger.py  write_log(): Something went wrong...")
--- tests/logger_test.py
+++ tests/logger_test.py
@@ -4,8 +4,11 @@ import os
 
 from batch_mailer import logger
 from os import path
+from datetime import datetime
 
 TEST_LOG_FOLDER_PATH = path.abspath("tests/logger_test_logs")
+
+logger.LOG_FOLDER_PATH = TEST_LOG_FOLDER_PATH
 
 
 class LoggerTests(unittest.TestCase):
@@ -19,56 +22,46 @@ class LoggerTests(unittest.TestCase):
             shutil.rmtree(TEST_LOG_FOLDER_PATH)
 
     def test_info(self):
-        logInfoMessage = "Test log info"
-        logger.info(logInfoMessage)
-
-        self.do_logger_assertions(logInfoMessage)
+        logger.info("This is an info log")
+        self.assert_log_type("INFO", "This is an info log")
 
     def test_warn(self):
-        logWarnMessage = "Test warn info"
-        logger.warn(logWarnMessage)
-
-        self.do_logger_assertions(logWarnMessage)
+        logger.warn("This is an warning log")
+        self.assert_log_type("WARN", "This is an warning log")
 
     def test_error(self):
-        logErrorMessage = "Test error info"
-        logger.error(logErrorMessage)
-
-        self.do_logger_assertions(logErrorMessage)
+        logger.error("This is an error log")
+        self.assert_log_type("ERROR", "This is an error log")
 
     def test_write_log(self):
         # Action
-        logger.write_log("Test log", TEST_LOG_FOLDER_PATH)
-        logger.write_log("Test log 2", TEST_LOG_FOLDER_PATH)
+        logger.write_log("Test log")
+        logger.write_log("There was an error!")
 
         # Assess
-        self.do_logger_assertions(self, "Test log")
-        self.do_logger_assertions(self, "Test log 2")
+        self.do_logger_assertions("Test log")
+        self.do_logger_assertions("There was an error!")
 
-    def assert_log_file_contents(self, contents):
-        pass
+    def assert_log_type(self, type: str, text):
+        log_time = datetime.now().strftime("%H:%M:%S")
+        log_message = f"[{type.upper()} {log_time}]: {text}\n"
 
-    def assert_log_file(self):
-        pass
+        self.do_logger_assertions(log_message)
 
     def do_logger_assertions(self, logText):
-        pass
-        # Something is wrong with this method:
+        if path.exists(TEST_LOG_FOLDER_PATH) == False:
+            self.fail(f"Log directory does not exist: '{TEST_LOG_FOLDER_PATH}'")
 
-        # if path.exists(TEST_LOG_FOLDER_PATH) == False:
-        #     self.fail(f"Log directory does not exist: '{TEST_LOG_FOLDER_PATH}'")
+        if os.listdir(TEST_LOG_FOLDER_PATH).count == 0:
+            self.fail("No log files created")
 
-        # if os.listdir(TEST_LOG_FOLDER_PATH).count == 0:
-        #     self.fail("No log files created")
+        logFileName = path.join(
+            TEST_LOG_FOLDER_PATH, os.listdir(TEST_LOG_FOLDER_PATH)[0]
+        )
 
-        # logFileName = path.join(
-        #     TEST_LOG_FOLDER_PATH, os.listdir(TEST_LOG_FOLDER_PATH)[0]
-        # )
+        with open(logFileName, "r") as logFile:
+            for line in logFile.readlines():
+                if logText in line:
+                    return
 
-        # with open(logFileName, "r") as logFile:
-        #     logFileContents = logFile.read()
-
-        #     if not logText in logFileContents:
-        #         self.fail(
-        #             f"Expected: '{logText}', but found the following in log file: '{logFileContents}'"
-        #         )
+            self.fail(f"Unable to find line with content: '{logText}'")
