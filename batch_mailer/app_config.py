fcc6514d0a8ade2d1ca704bcafc5734277c16ad9
Jason Azevedo <jasonazevedo03@gmail.com>
2023-08-31 21:09:04 +0200

    Implemented most of the app config tests

---
+++ batch_mailer/app_config.py
@@ -0,0 +1,15 @@
+import configparser, os
+
+CONFIG_PATH = "test_config.ini"
+
+
+def load_config():
+    pass
+
+
+def generate_config():
+    pass
+
+
+def validate_config(config):
+    pass
--- batch_mailer/config.py
+++
@@ -1,9 +0,0 @@
-import configparser, os
-
-def load_config():
-    return
-
-
-def generate_config():
-    return
-
