7d1f52b3915b7efdd0521f8e831d471bd018bbc8
Jason Azevedo <jasonazevedo03@gmail.com>
2023-09-01 20:45:19 +0200

    Config loading in program startup

--- batch_mailer/app_config.py
+++ batch_mailer/app_config.py
@@ -1,6 +1,6 @@
 import configparser, os
 
-CONFIG_PATH = "test_config.ini"
+CONFIG_PATH = "batch_mailer.ini"
 
 EMPTY_FROM_EMAIL_MSG = "From email cannot be empty"
 EMPTY_EMAIL_SUBJECT_MSG = "Email subject cannot be empty"
@@ -8,6 +8,9 @@ EMPTY_EMAIL_TEMPLATE_MSG = "Email template cannot be empty"
 
 
 def load_config():
+    if not os.path.isfile(os.path.abspath(CONFIG_PATH)):
+        generate_config()
+
     config = configparser.ConfigParser()
     config.read(os.path.abspath(CONFIG_PATH))
 
@@ -21,7 +24,6 @@ def generate_config():
         "EmailSubject": "",
         "EmailTemplate": "",
         "EmailAttachments": "",
-        "EmailListPath": "",
     }
 
     with open(os.path.abspath(CONFIG_PATH), "w") as config_file:
--- batch_mailer/batch_mailer.py
+++ batch_mailer/batch_mailer.py
@@ -3,13 +3,33 @@ import logger
 import app_config
 import os
 
+
+def validate_config(config):
+    validation_messages = app_config.validate_config(config)
+
+    if validation_messages.count == 0:
+        return True
+
+    for message in validation_messages:
+        logger.error(f"Config error: {message}")
+
+    return False
+
+
 # Prevent execution of this file if it is not the main script
 if __name__ != "__main__":
     exit()
 
+
 logger.info("STARTING RUN")
 
 # Load the config file
+config = app_config.load_config()
+config_valid = validate_config(config)
+
+if config_valid:
+    # Send mails
+    pass
 
 
 logger.info("RUN COMPLETE\n\n")
