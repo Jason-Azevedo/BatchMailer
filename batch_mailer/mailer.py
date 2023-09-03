059a01a42c9d86a7cafd807ad2073cb9fe332623
Jason Azevedo <jasonazevedo03@gmail.com>
2023-09-03 19:43:22 +0200

    Implemented test method for absolute attachment paths

--- batch_mailer/mailer.py
+++ batch_mailer/mailer.py
@@ -1,15 +1,66 @@
 import yagmail
 
+from batch_mailer import logger
 from os import path
+
 
 EMAIL_TEMPLATE_PATH = "email_templates"
 EMAIL_ATTACHMENTS_PATH = "email_attachments"
 
 EMAIL_LIST_PATH = "data/emails.csv"
-ACTIONED_EMAILS_PATH = "data/actioned_emails.txt"
+ACTIONED_EMAILS_PATH = "data/actioned_emails.csv"
 
 
 def send_mails(config):
-    # send the emails
+    try:
+        emailConfig = config["EMAIL"]
+        generalConfig = config["GENERAL"]
 
-    return
+        yag = yagmail.SMTP(
+            user=emailConfig["UserEmail"], password=emailConfig["UserPassword"]
+        )
+
+        emails = load_recipient_emails()
+        attachments = get_absolute_attachment_paths()
+        template = get_email_template()
+
+        # send emails
+        yag.send(
+            to=emails,
+            subject=emailConfig["EmailSubject"],
+            attachments=attachments,
+            content=template,
+        )
+
+        # Log actioned emails
+        mark_emails_as_actioned()
+
+        if generalConfig["ClearEmailsAfterRun"] == "Y":
+            clear_recipient_emails()
+
+    except Exception as err:
+        logger.error(
+            "An error occured in send_mails mailer.py. See below for more info"
+        )
+        logger.error(err)
+
+
+def load_recipient_emails():
+    pass
+
+
+def get_absolute_attachment_paths(config):
+    pass
+
+
+def get_email_template(config):
+    pass
+
+
+def mark_emails_as_actioned(config, emails):
+    # TODO: With the following columns: actioned_date, email, subject, template_name, attachments
+    pass
+
+
+def clear_recipient_emails():
+    pass
--- tests/mailer_test.py
+++ tests/mailer_test.py
@@ -1,5 +1,35 @@
 import unittest
 
+from batch_mailer import mailer
+from os import path
+
+TEST_ATTACHMENT_FOLDER = "tests/attachments"
+
+
 class MailerTest(unittest.TestCase):
-    def test_send_mail():
+    def test_load_recipient_emails(self):
+        pass
+
+    def test_get_absolute_attachment_paths(self):
+        config = {"EMAIL": {"Attachments": ["test.txt", "test2.txt"]}}
+
+        # Get the expected absolute email paths
+        expected_attachment_paths = []
+
+        for attachment in config["EMAIL"]["Attachments"]:
+            absolute_path = path.abspath(path.join(TEST_ATTACHMENT_FOLDER, attachment))
+            expected_attachment_paths.append(absolute_path)
+
+        mailer.EMAIL_ATTACHMENTS_PATH = TEST_ATTACHMENT_FOLDER
+        attachment_paths = mailer.get_absolute_attachment_paths(config)
+
+        self.assertEqual(expected_attachment_paths, attachment_paths)
+
+    def test_load_email_template(self):
+        pass
+
+    def test_mark_emails_as_actioned(self):
+        pass
+
+    def test_clear_recipient_emails(self):
         pass
