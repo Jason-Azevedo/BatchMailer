4d6e0395c7c5535416db9b5ba1871989393e40ec
Jason Azevedo <jasonazevedo03@gmail.com>
2023-09-04 06:34:58 +0200

    Implemented load_email_template test method

--- tests/mailer_test.py
+++ tests/mailer_test.py
@@ -3,7 +3,17 @@ import unittest
 from batch_mailer import mailer
 from os import path
 
-TEST_ATTACHMENT_FOLDER = "tests/attachments"
+TEST_TEMPLATE_FOLDER = path.abspath("tests/templates")
+TEST_ATTACHMENT_FOLDER = path.abspath("tests/attachments")
+
+
+TEST_APP_CONFIG = {
+    "GENERAL": {"ClearEmailsAfterRun": "Y"},
+    "EMAIL": {
+        "Template": "test_template.html",
+        "Attachments": ["test.txt", "test2.txt"],
+    },
+}
 
 
 class MailerTest(unittest.TestCase):
@@ -11,25 +21,36 @@ class MailerTest(unittest.TestCase):
         pass
 
     def test_get_absolute_attachment_paths(self):
-        config = {"EMAIL": {"Attachments": ["test.txt", "test2.txt"]}}
-
         # Get the expected absolute email paths
         expected_attachment_paths = []
 
-        for attachment in config["EMAIL"]["Attachments"]:
-            absolute_path = path.abspath(path.join(TEST_ATTACHMENT_FOLDER, attachment))
+        for attachment in TEST_APP_CONFIG["EMAIL"]["Attachments"]:
+            absolute_path = path.join(TEST_ATTACHMENT_FOLDER, attachment)
             expected_attachment_paths.append(absolute_path)
 
         mailer.EMAIL_ATTACHMENTS_PATH = TEST_ATTACHMENT_FOLDER
-        attachment_paths = mailer.get_absolute_attachment_paths(config)
+        attachment_paths = mailer.get_absolute_attachment_paths(TEST_APP_CONFIG)
 
         self.assertEqual(expected_attachment_paths, attachment_paths)
 
     def test_load_email_template(self):
-        pass
+        # Load the test template
+        template_text = ""
+        with open(
+            path.join(TEST_TEMPLATE_FOLDER, "test_template.html"), "r"
+        ) as template_file:
+            template_text = template_file.read()
+
+        template = mailer.get_email_template(TEST_APP_CONFIG)
+
+        self.assertEqual(template_text, template)
 
     def test_mark_emails_as_actioned(self):
-        pass
+        actioned_emails = ["test@mail.com", "test2@outlook.com", "test3@something.io"]
+
+        mailer.mark_emails_as_actioned(TEST_APP_CONFIG, actioned_emails)
+
+        # load the csv and check if the 3 entries are correct
 
     def test_clear_recipient_emails(self):
         pass
